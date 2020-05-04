3e2bbe351c6072e0d2a5342e618bd0ef327ab822
Tommy Anfosso <anfossotommy@hotmail.fr>
2020-05-04 16:48:21 +0200

    a priori abrecouvrant OK

--- src/arbres.cpp
+++ src/arbres.cpp
@@ -56,7 +56,7 @@ void graphe::affichage()
 		for(int i = 0; i < n; i++){
 			for(int j = 0; j < (n-1); j++)
 				cout << E[i][j] << "\t";
-			cout << E[i][n-1] << endl;		
+			cout << E[i][n-1] << endl;
 		}
 	}
 	else{
@@ -97,10 +97,10 @@ void graphe::arbrecouvrant()
 	vector<pair<int,int>> omega_C;
 	pair<int,int> e_etoile;
 	/* les valeurs de A sont déjà init à false, donc A vide */
-	int k = 0;
 
 	// C.push_back(0);	//on ajoute a la liste de sommets couverts le premier sommet
-	while(k != this->n - 1)	//autant faire une boucle for ??
+	for(int k = 1 ; k < this->n ; ++k)
+	// for(int k = 1 ; k <= 2 ; ++k)
 	{
 		// remplir omega_C et attribuer val a e etoile
 		int j = 0;
@@ -120,7 +120,9 @@ void graphe::arbrecouvrant()
 				bool C_contains_j = find(C.begin(), C.end(), j) != C.end();
 				if(!C_contains_i != !C_contains_j)
 				{
-					omega_C.push_back(make_pair(i,j));
+					//si ce n'est pas déjà dans l'ensemble
+					if(find(omega_C.begin(), omega_C.end(), make_pair(i,j)) == omega_C.end())
+						omega_C.push_back(make_pair(i,j));
 					if(this->E[i][j] < poidsArete)
 					{
 						e_etoile = make_pair(i, j); //on met dans e etoile l'arete au poids le plus petit
@@ -129,7 +131,7 @@ void graphe::arbrecouvrant()
 				}	
 			}
 		}
-
+		
 		this->A[e_etoile.first][e_etoile.second] = true;
 
 		//si l'extremite i n'est pas comprise dans C..
@@ -138,6 +140,38 @@ void graphe::arbrecouvrant()
 		else
 			C.push_back(e_etoile.second);
 
-		++k;
+		// Retirer e etoile de omega c
+		omega_C.erase(find(omega_C.begin(), omega_C.end(), e_etoile));
+		
+		// Ajouter toutes les aretes partant du nouveau sommet dans C
+		for(int i = 0 ; i < this->n ; ++i)
+			if(this->E[C.back()][i] != 0)
+				omega_C.push_back(make_pair(C.back(), i));
+
+		// retirer l'arete e etoile qui part du nouveau sommet dans C
+		omega_C.erase(find(omega_C.begin(), omega_C.end(), make_pair(e_etoile.second, e_etoile.first)));
+
+		cout<<"\ndebug iteration "<<k<<endl;
+
+		cout<<"C = "<<endl;
+		for(int i : C)
+			cout<<i<<endl;
+
+		cout<<endl;
+		cout<<"omega C ="<<endl;
+		for(pair<int,int> p : omega_C)
+			cout<<p.first<<";"<<p.second<<endl;
+
+		cout<<"e etoile = "<<endl;
+		cout<<endl<<e_etoile.first<<";"<<e_etoile.second<<endl;
+
+		cout<<"A="<<endl;
+		int v, w;
+		for( v = 0 ; v < this->n ; ++v)
+		{
+			for(w = 0 ; w < this->n - 1 ; ++w)
+				cout<<this->A[v][w]<<"\t";
+			cout<<this->A[v][w]<<endl;
+		}
 	}
 }
